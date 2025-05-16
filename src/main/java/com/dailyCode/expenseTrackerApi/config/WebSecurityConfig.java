3a0fa85690afcc813b252bbede3782fb305a49e3
sunil-gudle <sunilgudle7844@gmail.com>
2025-05-17 00:16:10 +0530

    Updated the code

--- src/main/java/com/dailyCode/expenseTrackerApi/config/WebSecurityConfig.java
+++ src/main/java/com/dailyCode/expenseTrackerApi/config/WebSecurityConfig.java
@@ -2,8 +2,6 @@ package com.dailyCode.expenseTrackerApi.config;
 
 import com.dailyCode.expenseTrackerApi.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,12 +9,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
